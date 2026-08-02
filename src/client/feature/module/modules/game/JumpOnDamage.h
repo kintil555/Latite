031a49084649c697997090af40be2335a477e4e0
kintil555 <dzikriganteng52637@gmail.com>
2026-08-02 11:39:38 +0700

    fix: JumpOnDamage.h - add m_jumpQueued field

--- src/client/feature/module/modules/game/JumpOnDamage.h
+++ src/client/feature/module/modules/game/JumpOnDamage.h
@@ -11,8 +11,8 @@ public:
 private:
     ValueType delay = IntValue(0);
 
-    float m_lastHealth     = -1.f;  // -1 = belum init
-    int   m_jumpTicksLeft  = 0;
-    bool  m_pendingJump    = false;
-    bool  m_waitingForReset = false;
+    float m_lastHealth    = -1.f;  // -1 = belum init
+    int   m_jumpTicksLeft = 0;
+    bool  m_pendingJump   = false;
+    bool  m_jumpQueued    = false;  // kunci: satu jump per hit event
 };
