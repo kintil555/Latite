8b598bbe5ff5bbadd2413b1fa08c24fc964918ed
kintil555 <dzikriganteng52637@gmail.com>
2026-08-08 12:21:50 +0700

    Fix DisableParticles.h content (previous commit pushed placeholder text)

--- src/client/feature/module/modules/game/DisableParticles.h
+++ src/client/feature/module/modules/game/DisableParticles.h
@@ -1,1 +1,14 @@
-PLACEHOLDER
+#pragma once
+#include "../../Module.h"
+
+class DisableParticles : public Module {
+public:
+    DisableParticles()
+        : Module("DisableParticles", LocalizeString::get("client.module.disableParticles.name"),
+                 LocalizeString::get("client.module.disableParticles.desc"), GAME) {}
+
+    ~DisableParticles() override {}
+
+    void onEnable() override;
+    void onDisable() override;
+};
