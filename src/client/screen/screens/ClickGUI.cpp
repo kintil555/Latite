8d62651a40133f5916c24b66bd86d1955870cf6f
kintil555 <dzikriganteng52637@gmail.com>
2026-08-08 06:12:23 +0000

    fix(ClickGUI): declare missing colorDisplayRect in color picker rendering (C2065)

--- src/client/screen/screens/ClickGUI.cpp
+++ src/client/screen/screens/ClickGUI.cpp
@@ -2004,6 +2004,8 @@ void ClickGUI::drawColorPicker() {
                                     totalDisplayRect.bottom };
             RectF hexBox = { colorModeRect.right + pad, totalDisplayRect.top, colorModeRect.right + pad + hexBoxWidth,
                              totalDisplayRect.bottom };
+            RectF colorDisplayRect = { hexBox.right + pad, totalDisplayRect.top, hexBox.right + pad + colorDisplayWidth,
+                                       totalDisplayRect.bottom };
 
             if (pickerTextBoxes.size() <= i) {
                 pickerTextBoxes.insert(pickerTextBoxes.begin() + i, TextBox(hexBox, 7));
